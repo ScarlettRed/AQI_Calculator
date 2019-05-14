947cfb438b8ee9b16ede33e4d37266e748074114
shivam-kapila <shivamkapila4@gmail.com>
2019-05-14 15:10:48 +0530

    Second Commit

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var express = require("express"),
     app = express(),
     bodyParser = require("body-parser");
 
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + "/public"));
 
-app.get ('/', () => {
-    res.render('home');
+
+app.get ('/', (req, res) => {
+    res.render('home', {value: null, verified: "false"});
 });
 
-app.post ('/', () => {
-
+app.post('/', (req, res) => {
+    res.redirect('home', {verified: "true", value: value});
 });
 
 app.listen(3000, process.env.IP, () => {
